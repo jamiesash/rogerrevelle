66e6a6aee49dce4b98cf2e8e7a478c68c8951a0a
jamiesash <jamesash@hawaii.edu>
2024-06-09 09:46:03 -1000

    good

--- code/cruise_animation.py
+++ code/cruise_animation.py
@@ -26,6 +26,18 @@ def readrbins(pth, sensor, tag):
     mat = np.array(mat) #, dtype=cols)
     return(mat)
 
+def findframe(gps, point):
+    A = gps[:,0] > point[0] - 0.01
+    B = gps[:,0] < point[0] + 0.01 
+    C = gps[:,1] > point[1] - 0.01
+    D = gps[:,1] < point[1] + 0.01
+    a = np.logical_and(A , B)
+    b = np.logical_and(C , D)
+    c = np.logical_and(a , b)
+    idx = np.where(c)
+    frame = np.min(idx)
+    return(frame)
+
 # Sea-Surface Temperature, NOAA Geo-polar Blended Analysis Day+Night, GHRSST, Near Real-Time, Global 5km, 2019-Present, Daily 
 file_id = Dataset('/home/jamie/projects/rogerrevelle/data/noaacwBLENDEDsstDNDaily_e5b2_b4c7_9276_U1717797277907.nc')
 # pull variables from nc file. 
@@ -60,27 +72,30 @@ sea = sea[0::500]
 x = sea[:, 2]
 y = sea[:, 3]
 s = sea.shape
+pos = np.array([x, y]).T
 
 tmp = np.empty((5,2,))
 tmp[:] = np.nan
 
-blur = np.linspace(0,1, 4)
+blur = np.linspace(0,1, 5)
 
 fig, (ax1) = plt.subplots(1, 1, figsize=(15, 10))
 ax1.contourf(ras.y, ras.x, ras[:, :], 100, cmap = "coolwarm")
 ax1.contourf(mask.y, mask.x, mask[:,:], 1, colors = "black")
 # Once I have position I should be fine with heading from the gyro. 
 ax1.grid(color = "grey", linestyle = '--', alpha = 0.6)# visible=None)
-c = ax1.contourf(ras.y, ras.x, ras[:, :], 100, cmap = "coolwarm")
-cbar = fig.colorbar(c)
+cm = ax1.contourf(ras.y, ras.x, ras[:, :], 100, cmap = "coolwarm")
+cbar = fig.colorbar(cm)
 scat = ax1.scatter(x[0], y[0], s = 100, alpha = blur, color = "black", label='Roger Revelle')
 # ax1.scatter(prev_pos[:,2], prev_pos[:,3], marker = ',', color = "black", s = 0.5, alpha = 0.5)
-pion1 = ax1.scatter(tmp[:,0], tmp[:,1], color = "grey", marker = 'X', s = 100, label='Pioneer Pie')
-pion2 = ax1.scatter(tmp[:,0], tmp[:,1], color = "grey", marker = 'X', s = 100, label='Pioneer Pie')
+pion1 = ax1.scatter(tmp[0,0], tmp[0,1], color = "grey", marker = 'X', s = 100, label='Pioneer Pie')
+pion2 = ax1.scatter(tmp[0,0], tmp[0,1], color = "red", marker = 'X', s = 100, label='Pioneer Pie')
 
 swot1 = ax1.scatter(swot_pos[0,0], swot_pos[0,1], color = "black", marker = 'X', s = 100, label='SWOT Pie')
 swot2 = ax1.scatter(swot_pos[1,0], swot_pos[1,1], color = "black", marker = 'X', s = 100, label='SWOT Pie')
 swot3 = ax1.scatter(swot_pos[2,0], swot_pos[2,1], color = "black", marker = 'X', s = 100, label='SWOT Pie')
+swot4 = ax1.scatter(swot_pos[3,0], swot_pos[3,1], color = "black", marker = 'X', s = 100, label='SWOT Pie')
+swot5 = ax1.scatter(swot_pos[4,0], swot_pos[4,1], color = "black", marker = 'X', s = 100, label='SWOT Pie')
 
 ax1.scatter(waypoints[:,0], waypoints[:,1], color = "red", marker = '^', s = 100, label='Waypoints')
 # cbar.set_label("Sea Surface Temperature [C$^\circ$]")
@@ -90,21 +105,21 @@ ax1.legend(loc = 'upper right')
 
 def update(frame):
     # for each frame, update the data stored on each artist.
-    u = x[(frame-4):frame]
-    v = y[(frame-4):frame]
+    u = x[(frame-5):frame]
+    v = y[(frame-5):frame]
     # update the scatter plot:
     data = np.stack([u, v]).T
     scat.set_offsets(data)
     
     # Deployed
-    if frame > 100:
+    if frame > findframe(gps = pos, point = pioneer_pos[0,:]):
         w = pioneer_pos[0,0]
         z = pioneer_pos[0,1]
         # update the scatter plot:
         data = np.stack([w, z]).T
         pion1.set_offsets(data)
 
-    if frame > 80:
+    if frame > findframe(gps = pos, point = pioneer_pos[1,:]):
         w = pioneer_pos[1,0]
         z = pioneer_pos[1,1]
         # update the scatter plot:
@@ -112,26 +127,40 @@ def update(frame):
         pion2.set_offsets(data)
 
     # Retreaved
-    if frame > 100:
-            w = tmp[0,0]
-            z = tmp[0,0]
-            # update the scatter plot:
-            data = np.stack([w, z]).T
-            swot1.set_offsets(data)
+    if frame > findframe(gps = pos, point = swot_pos[0,:]):
+        w = tmp[0,0]
+        z = tmp[0,0]
+        # update the scatter plot:
+        data = np.stack([w, z]).T
+        swot1.set_offsets(data)
     
-    if frame > 110:
+    if frame > findframe(gps = pos, point = swot_pos[1,:]):
         w = tmp[0,0]
         z = tmp[0,0]
         # update the scatter plot:
         data2 = np.stack([w, z]).T
         swot2.set_offsets(data2)
 
-    if frame > 120:
+    if frame > findframe(gps = pos, point = swot_pos[2,:]):
         w = tmp[0,0]
         z = tmp[0,0]
         # update the scatter plot:
         data3 = np.stack([w, z]).T
         swot3.set_offsets(data3)
+
+    if frame > findframe(gps = pos, point = swot_pos[3,:]):
+        w = tmp[0,0]
+        z = tmp[0,0]
+        # update the scatter plot:
+        data3 = np.stack([w, z]).T
+        swot4.set_offsets(data3)
+
+    if frame > findframe(gps = pos, point = swot_pos[4,:]):
+        w = tmp[0,0]
+        z = tmp[0,0]
+        # update the scatter plot:
+        data3 = np.stack([w, z]).T
+        swot5.set_offsets(data3)
 
     # update the line plot:
     #return(scat)
